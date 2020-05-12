fcae024c86c358dfae75e30ef7d1855b4e28e5a2
breenbo <berrehuelbruno@gmail.com>
2020-05-12 17:22:26 +0200

    lighthouse

--- src/components/modules/svg/upwork.vue
+++ src/components/modules/svg/upwork.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- upwork{{{1 -->
-  <q-btn
+  <q-img
     class="col"
     :class="$q.screen.gt.md ? 'bigBouton' : 'smallBouton'"
     fab
@@ -19,70 +19,70 @@
         fill="#6fda44"
       />
     </svg>
-  </q-btn>
+  </q-img>
   <!-- }}} -->
 </template>
 
 <script lang="ts">
-//Imports{{{1
-import { Vue, Component } from 'vue-property-decorator';
-//}}}
+  //Imports{{{1
+  import { Vue, Component } from 'vue-property-decorator';
+  //}}}
 
-//{{{1. props
-//const taskProp = Vue.extend({
-//props: ['task', 'id']
-//});
-//}}}
-
-@Component({
-  //declare components{{{1
-  //}}}
-  //filter{{{1
-  //filters: {
-  //niceDate(value: string): string {
-  //return date.formatDate(value, 'Do MMMM');
-  //}
-  //// TIPS : filter must return something
-  //}
-  //}}}
-})
-export default class Upwork extends Vue {
-  //props{{{1
-  //@Prop(String) readonly someProp!: string
-  //}}}
-  //datas{{{1
-  // get the store from TaskStore
-  //store = getModule(TaskStore);
-  // somedata:string = "some"
-  //}}}
-  //methods{{{1
-  //submitForm(): void {
-  //// send taskToSubmit to store
-  //this.store.updateTasksAction({
-  //id: this.id,
-  //updates: this.taskToSubmit
+  //{{{1. props
+  //const taskProp = Vue.extend({
+  //props: ['task', 'id']
   //});
-  //}
   //}}}
-  //computed{{{1
-  // get data from store : computed method
-  // don't use getter because no manipulation before get it
-  // use getters to manipulate datas before getting them
-  //get searchField(): string {
-  //return this.store.searchField;
-  //}
-  //set searchField(term: string) {
-  //this.store.setSearchFieldAction(term);
-  //}
-  //}}}
-  //hooks{{{1
-  //mounted() {
-  //somefunction: () => {
-  //return something
-  //}
-  //}
-  //}}}
-}
+
+  @Component({
+    //declare components{{{1
+    //}}}
+    //filter{{{1
+    //filters: {
+    //niceDate(value: string): string {
+    //return date.formatDate(value, 'Do MMMM');
+    //}
+    //// TIPS : filter must return something
+    //}
+    //}}}
+  })
+  export default class Upwork extends Vue {
+    //props{{{1
+    //@Prop(String) readonly someProp!: string
+    //}}}
+    //datas{{{1
+    // get the store from TaskStore
+    //store = getModule(TaskStore);
+    // somedata:string = "some"
+    //}}}
+    //methods{{{1
+    //submitForm(): void {
+    //// send taskToSubmit to store
+    //this.store.updateTasksAction({
+    //id: this.id,
+    //updates: this.taskToSubmit
+    //});
+    //}
+    //}}}
+    //computed{{{1
+    // get data from store : computed method
+    // don't use getter because no manipulation before get it
+    // use getters to manipulate datas before getting them
+    //get searchField(): string {
+    //return this.store.searchField;
+    //}
+    //set searchField(term: string) {
+    //this.store.setSearchFieldAction(term);
+    //}
+    //}}}
+    //hooks{{{1
+    //mounted() {
+    //somefunction: () => {
+    //return something
+    //}
+    //}
+    //}}}
+  }
 </script>
 
 <style scoped>
