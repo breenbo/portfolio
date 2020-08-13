35b5bdbe510469a16a3fb28a1ca593414d259baa
breenbo <berrehuelbruno@gmail.com>
2020-08-13 09:40:48 +0200

    add fixatable

--- src/components/modules/carousel.vue
+++ src/components/modules/carousel.vue
@@ -2,16 +2,25 @@
   <!-- TODO : add link to projects pages -->
   <div class="q-pa-xs">
     <!-- bigCarousel{{{1 -->
-    <div class="row" v-if="$q.screen.gt.sm">
+    <div
+      class="row"
+      v-if="$q.screen.gt.sm"
+    >
       <div
         class="col roundedCorner imageContainer"
         :class="$q.screen.gt.md ? 'imageContainerBig' : 'imageContainerMedium'"
       >
         <!-- left container for video, img or text -->
         <transition enter-active-class="animated fadeIn">
-          <div class="row items-center full-height" v-if="slides[slide].video">
+          <div
+            class="row items-center full-height"
+            v-if="slides[slide].video"
+          >
             <transition enter-active-class="animated fadeIn">
-              <div class="col" :key="slides[slide].name">
+              <div
+                class="col"
+                :key="slides[slide].name"
+              >
                 <q-media-player
                   type="video"
                   :source="slides[slide].video"
@@ -71,7 +80,10 @@
             :name="slide.name"
           >
             <div class="q-mt-xl q-ma-md">
-              <div :class="fontSize" class="text-center">
+              <div
+                :class="fontSize"
+                class="text-center"
+              >
                 {{ slide.title }}
               </div>
               <div
@@ -93,7 +105,10 @@
           <div class="text-center text-h6 q-mb-md">
             {{ slides[slide].title }}
           </div>
-          <div class="row items-center full-height" v-if="slides[slide].video">
+          <div
+            class="row items-center full-height"
+            v-if="slides[slide].video"
+          >
             <div class="col">
               <q-media-player
                 dense
@@ -111,7 +126,10 @@
             </div>
           </div>
           <div v-else-if="slides[slide].image">
-            <a :href="slides[slide].link" target="_blank">
+            <a
+              :href="slides[slide].link"
+              target="_blank"
+            >
               <q-img
                 class="full-height roundedCorner"
                 :srcset="`${slides[slide].image}.webp`"
@@ -145,7 +163,10 @@
             :name="slide.name"
             class="no-wrap flex-center"
           >
-            <div class="text-caption" v-html="slide.text"></div>
+            <div
+              class="text-caption"
+              v-html="slide.text"
+            ></div>
           </q-carousel-slide>
         </q-carousel>
       </div>
@@ -155,111 +176,119 @@
 </template>
 
 <script lang="ts">
-  //Imports{{{1
-  import { Vue, Component } from 'vue-property-decorator';
-  import { DoProject } from 'components/models/models';
-  //}}}
+//Imports{{{1
+import { Vue, Component } from 'vue-property-decorator';
+import { DoProject } from 'components/models/models';
+//}}}
 
-  @Component
-  export default class Carousel extends Vue {
-    //datas{{{1
-    hideVolume = true;
-    slide = 'first';
-    slides: DoProject = {
-      first: {
-        name: 'first',
-        title: 'Portail BN',
-        text:
-          ' Complete Web App made with VueJS and Vuetify. Several pages, with specific content, theme color and animated svg for menu.<br/><span class="text-italic">Backend by excel sheet</span>, works surprisingly well.',
-        image: 'statics/img/projects/portailScreen',
-        link: '',
-        video: 'statics/videos/portail.webm'
-      },
-      second: {
-        name: 'second',
-        title: 'Tribute to Elon Musk',
-        text: 'Experimentation with verticals containers and sliders.',
-        image: 'statics/img/projects/elonScreen',
-        link: 'https://breenbo.github.io/FCC_tribute/',
-        video: 'statics/videos/elonTributeVideo.webm'
-      },
-      third: {
-        name: 'third',
-        title: 'Awesome ToDo',
-        text:
-          'Todo web and android single page app, made with VueJS and Quasar framework. Backend by Firebase. <br/>Differents user, local storage for personnal settings.',
-        image: 'statics/img/projects/todoScreen',
-        link: '',
-        video: 'statics/videos/todoApp.webm'
-      },
-      fourth: {
-        name: 'fourth',
-        title: 'Landing Page',
-        text:
-          'Full responsive landing page project made with VueJS and Quasar framework.',
-        image: 'statics/img/projects/landingPage',
-        link: 'https://landingpagetemplate.netlify.app/#/',
-        video: ''
-      },
-      fifth: {
-        name: 'fifth',
-        title: 'Meal App',
-        text:
-          'Cooking app made with VueJS and Vuetify framework, based on an API reached with axios. Backend by Firebase.',
-        image: 'statics/img/projects/mealScreen',
-        link: '',
-        video: 'statics/videos/mealApp.webm'
-      }
-      //fourth: {
-      //name: 'fifth',
-      //title: 'Vue-Gram',
-      //text:
-      //'Little chat made with Vue and Vuetify. Backend by Firebase. <br/> Different users.',
-      //image: '',
-      //link: '',
-      //video: ''
-      //},
-    };
-    //}}}
-    //computed{{{1
-    get fontSize(): string {
-      return this.$q.screen.gt.md ? 'text-h2' : 'text-h4';
-    }
-    //}}}
+@Component
+export default class Carousel extends Vue {
+  //datas{{{1
+  hideVolume = true;
+  slide = 'first';
+  slides: DoProject = {
+    first: {
+      name: 'first',
+      title: 'Portail BN',
+      text:
+        ' Complete Web App made with VueJS and Vuetify. Several pages, with specific content, theme color and animated svg for menu.<br/><span class="text-italic">Backend by excel sheet</span>, works surprisingly well.',
+      image: 'statics/img/projects/portailScreen',
+      link: '',
+      video: 'statics/videos/portail.webm',
+    },
+    second: {
+      name: 'second',
+      title: 'FixATable',
+      text:
+        'Responsive web app to book a table in cafes. Backend by Firebase. <br/>Differents users and cafe owner, manage availables tables in different cafes.',
+      image: 'statics/img/projects/fixatable',
+      link: '',
+      video: '',
+    },
+    third: {
+      name: 'third',
+      title: 'Awesome ToDo',
+      text:
+        'Todo web and android single page app, made with VueJS and Quasar framework. Backend by Firebase. <br/>Differents user, local storage for personnal settings.',
+      image: 'statics/img/projects/todoScreen',
+      link: '',
+      video: 'statics/videos/todoApp.webm',
+    },
+    fourth: {
+      name: 'fourth',
+      title: 'Landing Page',
+      text:
+        'Full responsive landing page project made with VueJS and Quasar framework.',
+      image: 'statics/img/projects/landingPage',
+      link: 'https://landingpagetemplate.netlify.app/#/',
+      video: '',
+    },
+    fifth: {
+      name: 'fifth',
+      title: 'Meal App',
+      text:
+        'Cooking app made with VueJS and Vuetify framework, based on an API reached with axios. Backend by Firebase.',
+      image: 'statics/img/projects/mealScreen',
+      link: '',
+      video: 'statics/videos/mealApp.webm',
+    },
+    sixth: {
+      name: 'sixth',
+      title: 'Tribute to Elon Musk',
+      text: 'Experimentation with verticals containers and sliders.',
+      image: 'statics/img/projects/elonScreen',
+      link: 'https://breenbo.github.io/FCC_tribute/',
+      video: 'statics/videos/elonTributeVideo.webm',
+    }, //fourth: {
+    //name: 'fifth',
+    //title: 'Vue-Gram',
+    //text:
+    //'Little chat made with Vue and Vuetify. Backend by Firebase. <br/> Different users.',
+    //image: '',
+    //link: '',
+    //video: ''
+    //},
+  };
+  //}}}
+  //computed{{{1
+  get fontSize(): string {
+    return this.$q.screen.gt.md ? 'text-h2' : 'text-h4';
   }
+  //}}}
+}
 </script>
 
 <style scoped>
-  .custom-caption {
-    background-color: hsla(0, 0%, 0%, 0.3);
-    color: hsl(0, 0%, 90%);
-  }
-  .roundedCorner {
-    border-radius: 8px;
-  }
-  .imageContainer {
-    position: absolute;
-    left: -98%;
-    top: -60px;
-    z-index: 2;
-    width: 95%;
-    background-color: hsl(256, 38%, 80%);
-  }
-  .imageContainerBig {
-    height: 170%;
-  }
-  .imageContainerMedium {
-    height: 140%;
-  }
-  .image {
-    height: 100%;
-    padding-top: 5%;
-    margin: 0 auto;
-  }
-  .q-carousel {
-    height: 350px;
-  }
-  .fadeIn {
-    transition-duration: 1000ms;
-  }
+.custom-caption {
+  background-color: hsla(0, 0%, 0%, 0.3);
+  color: hsl(0, 0%, 90%);
+}
+.roundedCorner {
+  border-radius: 8px;
+}
+.imageContainer {
+  position: absolute;
+  left: -98%;
+  top: -60px;
+  z-index: 2;
+  width: 95%;
+  background-color: hsl(256, 38%, 80%);
+}
+.imageContainerBig {
+  height: 170%;
+}
+.imageContainerMedium {
+  height: 140%;
+}
+.image {
+  height: 100%;
+  padding-top: 5%;
+  margin: 0 auto;
+}
+.q-carousel {
+  height: 350px;
+}
+.fadeIn {
+  transition-duration: 1000ms;
+}
 </style>
